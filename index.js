5022baf4038a3c4aaea15889cf4e10963d7e7f04
TonyPham-Dev <phamvancong300701@gmail.com>
2024-03-31 13:19:17 +0700

    fix proxy

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const puppeteer = require("puppeteer");
 const accounts = [
   // "GeraldBarn61920|CamChe@2022|3T5RZ6U2W7IUG4YI",
   // "CodyGarris16445|CamChe@2022|EBDAOUJTBIBYJU4Q",
-  "RosettaBec40908|CamChe@2022|CGH4O5K7PZSLBP5U",
-  "MatildaC90244|CamChe@2022|CNPV6K2ILTCO27LD",
-  "CobbBetty18228|CamChe@2022|C3A3D6H5RQ3NPZ4S",
-  "RalphBenso15819|CamChe@2022|FYE6DMMFIFP4PMK5",
+  // "RosettaBec40908|CamChe@2022|CGH4O5K7PZSLBP5U",
+  // "MatildaC90244|CamChe@2022|CNPV6K2ILTCO27LD",
+  // "CobbBetty18228|CamChe@2022|C3A3D6H5RQ3NPZ4S",
+  // "RalphBenso15819|CamChe@2022|FYE6DMMFIFP4PMK5",
   "DurhamNath52746|CamChe@2022|3DEDCNVEI4HELSRF",
   "GlenFlanna82073|CamChe@2022|A45J5M7VSPJ3J7EE",
   "josephine_70421|CamChe@2022|5MRDHFLCMQLOHOLJ",
@@ -26,14 +26,14 @@ const accounts = [
 const proxies = [
   "45.41.169.70:6731:xgptTop1VN:xgpt1688",
   // "104.239.91.205:5929:xgptTop1VN:xgpt1688",
-  "64.137.59.225:6818:xgptTop1VN:xgpt1688",
+  // "64.137.59.225:6818:xgptTop1VN:xgpt1688",
   "104.239.13.213:6842:xgptTop1VN:xgpt1688",
   "64.43.89.41:6300:xgptTop1VN:xgpt1688",
   "45.61.97.123:6649:xgptTop1VN:xgpt1688",
   "155.254.49.79:6639:xgptTop1VN:xgpt1688",
-  // "89.40.222.137:6513:xgptTop1VN:xgpt1688", //error
-  "84.33.224.177:6201:xgptTop1VN:xgpt1688",
-  "45.192.146.50:6061:xgptTop1VN:xgpt1688",
+  // "89.40.222.137:6513:xgptTop1VN:xgpt1688", 
+  // "84.33.224.177:6201:xgptTop1VN:xgpt1688",
+  // "45.192.146.50:6061:xgptTop1VN:xgpt1688",
   "64.137.8.112:6794:xgptTop1VN:xgpt1688",
   "104.249.29.205:5898:xgptTop1VN:xgpt1688",
   "150.107.224.165:6080:xgptTop1VN:xgpt1688",
@@ -63,16 +63,14 @@ async function loginTwitter(data, proxy) {
   });
 
   const page = await browser.newPage();
+  console.log("new page started")
   if (userNameProxy && passwordProxy) {
     await page.authenticate({
       username: userNameProxy,
       password: passwordProxy,
     });
   }
-  await page.goto("https://twitter.com/i/flow/login", {
-    waitUntil: "networkidle2",
-    timeout: 0,
-  });
+  await page.goto("https://twitter.com/i/flow/login");
   await page.waitForNetworkIdle(2000);
   await page.waitForNetworkIdle("[autocomplete=username]");
   await page.waitForNetworkIdle(2000);
@@ -92,10 +90,7 @@ async function loginTwitter(data, proxy) {
   );
 
   const page2Fa = await browser.newPage();
-  await page2Fa.goto("https://2fa.live", {
-    waitUntil: "networkidle2",
-    timeout: 0,
-  });
+  await page2Fa.goto("https://2fa.live");
   await page2Fa.waitForNetworkIdle(2000);
   await page2Fa.waitForNetworkIdle("textarea[id=listToken]");
   await page2Fa.type("textarea[id=listToken]", keyFa, {
@@ -125,20 +120,12 @@ async function loginTwitter(data, proxy) {
 }
 
 async function main() {
-  await runAccount(0);
-}
-
-async function runAccount(index) {
-  if (index < accounts.length) {
-    const account = accounts[index];
-    const proxy = proxies[index % proxies.length];
-    console.log({
-      account,
-      proxy,
-    });
+  for (let i = 0; i < accounts.length; i++) {
+    const account = accounts[i];
+    const proxy = proxies[i % proxies.length];
+    console.log(`Logging in with account ${i + 1}/${accounts.length}`);
     await loginTwitter(account, proxy);
-    await delay(2000);
-    await runAccount(index + 1); 
+    await delay(5000);
   }
 }
 
